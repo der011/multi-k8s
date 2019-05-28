530007ede31870ef9f457bc0bd4bc1d3f4be2391
Pavel Dergel <pdergel@Pavels-MacBook-Pro.local>
2019-05-28 11:19:44 +0200

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,8 @@ docker push der011/multi-worker:$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployments/client-deployment server=der011/multi-client:$SHA
+kubectl set image deployments/client-deployment client=der011/multi-client:$SHA
 kubectl set image deployments/server-deployment server=der011/multi-server:$SHA
-kubectl set image deployments/worker-deployment server=der011/multi-worker:$SHA
+kubectl set image deployments/worker-deployment worker=der011/multi-worker:$SHA
 
 
