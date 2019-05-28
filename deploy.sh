2bca5efc6455bcb2250ecb010dc554d500b82921
Pavel Dergel <pdergel@Pavels-MacBook-Pro.local>
2019-05-28 10:33:28 +0200

    Travis GC deploy configuration

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+docker build -t der011/multi-client:latest -t der011/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t der011/multi-server:latest -t der011/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t der011/multi-worker:latest -t der011/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push der011/multi-client:latest
+docker push der011/multi-server:latest
+docker push der011/multi-worker:latest
+
+docker push der011/multi-client:$SHA
+docker push der011/multi-server:$SHA
+docker push der011/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/client-deployment server=der011/multi-client:$SHA
+kubectl set image deployments/server-deployment server=der011/multi-server:$SHA
+kubectl set image deployments/worker-deployment server=der011/multi-worker:$SHA
+
+
